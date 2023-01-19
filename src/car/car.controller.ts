3eae6541c4dcff5c6e2b5e78440e7c6e77b13e64
ebradford <emma_bradford@milton.edu>
2023-01-18 19:21:25 -0500

    made get all cars for the service

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get  } from '@nestjs/common';
 import { CarService} from './car.service'
 
 @Controller('car')
@@ -6,7 +6,8 @@ export class CarController {
     constructor (private carService: CarService)
     {}
 
-    async getCars(){
-        this.carService.getCars();
+    @Get()
+    async getCars(): Promise<any>{
+        return this.carService.getCars();
     }
 }
