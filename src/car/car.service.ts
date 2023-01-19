19369dc31dfbc416896cd9984ab54eb305669d21
ebradford <emma_bradford@milton.edu>
2023-01-18 19:40:58 -0500

    filled out skeletons of basic crud service

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CARS }  from './cars.mock';
 
 @Injectable()
+
 export class CarService {
+    private cars = CARS;
+
     public async getCars(){
-        return{};  
+        return this.cars;  
+    }
+
+    public async postCar(car){
+        return;
+    }
+
+    public async getCarById(id){
+        return;
+    }
+
+    public async deleteCarById(id){
+        return;
+    }
+
+    public async putCarById(id){
+        return;
     }
 }
