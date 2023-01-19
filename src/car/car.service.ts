a5ae593111e8451126335b1f31855f584ca63dd7
ebradford <emma_bradford@milton.edu>
2023-01-18 19:29:58 -0500

    added summy data to the mock of car

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CarService {
     public async getCars(){
-        return{};
+        return{};  
     }
 }
---
+++ src/car/cars.mock.ts
@@ -0,0 +1,8 @@
+export const CARS = [
+    {
+        id:"123", 
+        brand: "BMW", 
+        color:"red", 
+        model: "400", 
+    }, 
+];
