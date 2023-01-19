19c8aacb71ef59ca468cfd9801ee16471de336a3
ebradford <emma_bradford@milton.edu>
2023-01-18 19:19:31 -0500

    made get all cars for the controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//import { AppController } from './app.controller';
+//import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
 
 @Module({
   imports: [CarModule],
-  controllers: [AppController],
-  providers: [AppService],
+  //controllers: [AppController],
+  //providers: [AppService],
 })
 export class AppModule {}
