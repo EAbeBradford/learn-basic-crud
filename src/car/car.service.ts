fcaa5bcfa21b061a2c73bb1bf49f8f42304da6e6
ebradford <emma_bradford@milton.edu>
2023-01-18 20:46:31 -0500

    added imports to service

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,6 +1,10 @@
 import { Injectable, HttpException } from '@nestjs/common';
 import { resolve } from 'path';
 import { CARS } from './cars.mock';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { ICar } from './interfaces/car.interface';
+import { CarDto } from './car.dto';
 
 @Injectable()
 export class CarService {
