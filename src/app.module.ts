be964f6557686dcb329f087c220d412571764c1f
ebradford <emma_bradford@milton.edu>
2023-01-18 20:43:16 -0500

    added car interface and schem

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 //import { AppController } from './app.controller';
 //import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [CarModule],
+  imports: [
+    MongooseModule.forRoot("mongodb://localhost/car_manager"),
+    CarModule],
   //controllers: [AppController],
   //providers: [AppService],
 })
