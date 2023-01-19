b6995714bc9ef449a47adedf6262772275d7bbcb
ebradford <emma_bradford@milton.edu>
2023-01-18 19:59:40 -0500

    filled out skeletons of basic crud controller

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get  } from '@nestjs/common';
+import { Controller, Get ,Post, Delete, Put } from '@nestjs/common';
 import { CarService} from './car.service'
 
 @Controller('car')
@@ -7,7 +7,28 @@ export class CarController {
     {}
 
     @Get()
-    async getCars(): Promise<any>{
+    async getCars(){
         return this.carService.getCars();
     }
+
+    @Get('/id')
+    async getCarsById(){
+        return this.carService.getCar();
+    }
+
+    @Post()
+    async postCar(){
+        return this.carService.postCarById();
+    }
+
+    @Delete()
+    async deleteCarById(){
+        return this.carService.deleteCarById();
+    }
+    
+    @Put()
+    async putCarById(){
+        return this.carService.putCarById();
+    }
+
 }
